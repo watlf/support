eb59dac3a8e1e266bbd225ae64ab5dd35aa2bc11
andrey <andrey.lyahovski@grandiz.com>
2015-09-14 10:35:17 +0300

    add user and admin panel with access

---
+++ database/migrations/2015_09_11_072913_create_views_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateViewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('views', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('question_id');
+            $table->integer('user_id');
+
+            $table->foreign('question_id')->references('id')->on('questions')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('views');
+    }
+}
